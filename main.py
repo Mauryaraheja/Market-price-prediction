e2d1b716c9c3c704f0696c5123e6e5600e731d6a
Mauryaraheja <mraheja_be24@thapar.edu>
2026-03-28 14:52:20 +0530

    I have made some changes in main file and created a new file linear regression , add that

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+#main.py - run this to execute the full pipeline
+from backtest import trades , wins , losses , win_rate , total_R
+from linear_regression import run_prediction 
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+if __name__ == "__main__":
+    print("=== Bactest Results ===")
+    print(f"WIN Rate : {win_rate:2f}% | total R: {total_R}")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("\n=== ML Prediction ===")
+    run_prediction()
